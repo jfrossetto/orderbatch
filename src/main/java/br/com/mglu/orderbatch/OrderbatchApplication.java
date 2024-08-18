908ed82dc7cdec190b4bc18b5d0f388ffd19182f
jebs <jfrossetto@gmail.com>
2024-08-18 19:44:33 -0300

    initial structure

---
+++ src/main/java/br/com/mglu/orderbatch/OrderbatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.mglu.orderbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mglu/orderbatch/OrderbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mglu.orderbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
